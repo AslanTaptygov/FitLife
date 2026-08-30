f6a8fe989de96eb438f5d35cf60ce783151b9915
Aslan Taptygov <aslantaptygov8@gmail.com>
2026-08-30 21:15:51 +0300

    Внесение правок ревьюера №1

--- fit_life.py
+++ fit_life.py
@@ -1,37 +1,56 @@
 # КОНСТАНТЫ:
-MILLILITERS_RECOMENDATION = 30  # рекомендуемое количество мл воды на кг
-MILLILITERS_PER_LITER = 1000  # количество мл в л
+MILLILITERS_RECOMENDATION = 30
+MILLILITERS_PER_LITER = 1000
+DIVING_LINE = 56
+
 
 # КОД:
-while True:
-    try:
-        print('Здравствуйте! Я фитнес-бот - Ваш помощник!')
+def program():
+    """
+    Бот для высчитывания ИМТ и нормы воды
+    >>>Запрашивает:
+    1) Имя пользователя (user_name)
+    2) Возраст пользователя (user_age)
+    3) Вес пользователя (user_weight)
+    4) Рост пользователя (user_height)
+    >>>Возвращает: ИМТ и норму потребления воды
+    >>>КОНСТАНТЫ:
+    1) MILLILITERS_RECOMENDATION - рекомендованное количество мл на кг веса
+    2) MILLILITERS_PER_LITER - количество мл в л
+    3) DIVING_LINE - нужна для создания разделительной черты
+       (если программа завершила работу с ошибкой)
+    """
+    while True:
+        try:
+            print('Здравствуйте! Я фитнес-бот - Ваш помощник!')
 
-        # Ввод имени пользователя:
-        user_name = input('Как мне к Вам обращаться? ')
-        user_name = user_name.title()
-        # Ввод возраста пользователя:
-        user_age = int(input('Сколько Вам лет? '))
+            user_name = input('Как мне к Вам обращаться? ')
+            user_name = user_name.title()
+            user_age = int(input('Сколько Вам лет? '))
 
-        print(f'Очень приятно с Вами познакомиться, {user_name}!')
+            print(f'Очень приятно с Вами познакомиться, {user_name}!')
 
-        # Ввод веса пользователя:
-        user_weight = float(input('Сколько Вы весите в кг? (например, 63.5) '))
-        # Ввод роста пользователя:
-        user_height = float(input('Какой у Вас рост в м? (например, 1.75) '))
+            user_weight = input('Сколько Вы весите в кг? ')
+            user_weight = float(user_weight.replace(',', '.'))
 
-        # Рассчёт индекса массы тела (ИМТ):
-        bmi = round(user_weight / (user_height ** 2), 1)
+            user_height = input('Какой у Вас рост в м? ')
+            user_height = float(user_height.replace(',', '.'))
 
-        # Рассчёт нормы воды (сначала в мл, потом в л):
-        water_ml = user_weight * MILLILITERS_RECOMENDATION
-        water_l = water_ml / MILLILITERS_PER_LITER
+            # Рассчёт индекса массы тела (ИМТ):
+            bmi = round(user_weight / (user_height ** 2), 1)
 
-        print(f'\nОтчёт для пользователя: {user_name} ({user_age} г.)')
-        print(f'Ваш индекс массы тела (ИМТ): {bmi}')
-        print(f'Рекомендуемая норма воды: {water_l:.1f} л в день')
-        print('\nРасчёт окончен. Будьте здоровы!')
-        break
-    except ValueError:
-        print('-' * 56)
-        print('Данные введены некорректно. Пожалуйста, попробуйте снова\n')
+            # Рассчёт нормы воды (сначала в мл, потом в л):
+            water_ml = user_weight * MILLILITERS_RECOMENDATION
+            water_l = water_ml / MILLILITERS_PER_LITER
+
+            print(f'\nОтчёт для пользователя: {user_name} ({user_age} г.)')
+            print(f'Ваш индекс массы тела (ИМТ): {bmi}')
+            print(f'Рекомендуемая норма воды: {water_l:.1f} л в день')
+            print('\nРасчёт окончен. Будьте здоровы!')
+            break
+        except ValueError:
+            print('-' * DIVING_LINE)
+            print('Данные введены некорректно. Пожалуйста, попробуйте снова\n')
+
+
+program()
